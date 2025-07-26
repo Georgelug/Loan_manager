89831c0e2d6315614ebea2a7d39dea2cca1f8215
Jorge Esparza <esparzajorge03@gmail.com>
2025-07-26 01:34:26 -0600

    Se crean todas las reglas de negocio requeridas

--- Utils/loan_manager_flow.py
+++ Utils/loan_manager_flow.py
@@ -26,6 +26,47 @@ def create_client(client: Client, db: Session) -> ClientModel:
         print(f"Error al crear el cliente: {e}")
         raise Exception(f"loan_manager_flow::create_client: No se pudo crear el cliente {client} en la base de datos, error: {e}")
 
+def get_loans_by_client_id(client_id: int, db: Session) -> list[Loan]:
+    try:
+        loans = db.query(LoanModel).filter(LoanModel.client_id == client_id).all()
+        if not loans:
+            raise Exception(f"No se encontraron préstamos para el cliente con id={client_id}")
+        
+        return [Loan(
+            id=loan.id,
+            client_id=loan.client_id,
+            amount=loan.amount,
+            fecha_otorgamiento=loan.fecha_otorgamiento,
+            status=loan.status
+        ) for loan in loans]
+    
+    except Exception as e:
+        raise Exception(f"loan_manager_flow::get_loans_by_client_id: Error al obtener préstamos del cliente {client_id}: {e}")
+
+
+
+def update_loan_status(loan_id: int, new_status: Status, db: Session) -> Loan:
+    try:
+        loan = db.query(LoanModel).filter(LoanModel.id == loan_id).first()
+        if not loan:
+            raise Exception(f"No se encontró un préstamo con id={loan_id}")
+        
+        loan.status = new_status
+        db.commit()
+        db.refresh(loan)
+
+        return Loan(
+            id=loan.id,
+            client_id=loan.client_id,
+            amount=loan.amount,
+            fecha_otorgamiento=loan.fecha_otorgamiento,
+            status=loan.status
+        )
+    
+    except Exception as e:
+        raise Exception(f"loan_manager_flow::update_loan_status: Error al actualizar el estado del préstamo {loan_id}: {e}")
+
+
 def create_loan(loan: Loan, db: Session):
     try:
         new_loan = LoanModel(
@@ -44,6 +85,7 @@ def create_loan(loan: Loan, db: Session):
         db.rollback()
         print(f"Error al crear préstamo: {e}")
         raise Exception(f"loan_manager_flow::create_loan: No se pudo crear el Prestamo {loan} en la base de datos, error: {e}")
+
 
 
 def main_flow():
@@ -68,12 +110,16 @@ def main_flow():
         status=Status.activo
     )
 
-    db.add(new_loan)
-    db.commit()
-    db.refresh(new_loan)
-    result_loan = new_loan
+    result_loan = create_loan(loan=new_loan,db=db)
     
     print(f"Prestamo otorgado: {result_loan}")
+
+    result_loans = get_loans_by_client_id(client_id=result_client.id,db=db)
+
+    print(f"prestamos de cliente {result_client.id},\n{result_loans}")
+
+    updated_loan = update_loan_status(loan_id=result_loan.id, new_status=Status.pagado, db=db)
+    print(updated_loan)
 
 
 
